b7645cacdb4d4a27b6902a4afa025467a401de57
PESOKOTiK <alexey.pylypenko007@gmail.com>
2025-06-12 16:11:20 +0000

    changed webconfig cors

--- src/main/java/backend/todo/todobackend/web/WebConfig.java
+++ src/main/java/backend/todo/todobackend/web/WebConfig.java
@@ -1,16 +1,12 @@
-package backend.todo.todobackend.web;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Dla wszystkich ścieżek
-                .allowedOrigins("http://localhost:3000") // Tylko localhost:3000
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Dozwolone metody HTTP
-                .allowCredentials(true); // Zezwolenie na przesyłanie ciasteczek i nagłówków autoryzacji
+        registry.addMapping("/**")
+                .allowedOrigins("*") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+                
     }
 }
+
