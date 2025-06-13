d4b55bc9205ea9ec51fcec2b34c809502072bd34
Alexey Pylypenko <alexey.pylypenko007@gmail.com>
2025-06-14 02:28:00 +0300

    Changed local time to vilnius

--- src/main/java/backend/todo/todobackend/TodoBackendApplication.java
+++ src/main/java/backend/todo/todobackend/TodoBackendApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class TodoBackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Vilnius"));
         SpringApplication.run(TodoBackendApplication.class, args);
     }
 
